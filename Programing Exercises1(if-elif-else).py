1cc9a3361c697f88d95b45fa74cc2440bdd502ed
WenLong <hkivanleeon68sem2@gmail.com>
2017-10-30 01:44:49 +0800

    If-Elif-Else
    Day of week

---
+++ Programing Exercises1(if-elif-else).py
@@ -0,0 +1,31 @@
+# Programming exercises
+# 1.Day of the week
+print("***Day of the week***")
+
+# Get number range 1-7
+Date = int(input("Please enter 1-7: "))
+
+# Determine which number represent which date
+if (Date == 1):
+    print("Monday")
+
+elif (Date == 2):
+    print("Tuesday")
+
+elif (Date == 3):
+    print("Wednesday")
+
+elif (Date == 4):
+    print("Thursday")
+
+elif (Date == 5):
+    print("Friday")
+
+elif (Date == 6):
+    print("Saturday")
+
+elif (Date == 7):
+    print("Sunday")
+
+else:
+    print("Sorry, please enter again.")
