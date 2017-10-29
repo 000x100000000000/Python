3d5f5124c62715bab1f19e1fe766c1411ed953d0
WenLong <hkivanleeon68sem2@gmail.com>
2017-10-29 09:35:52 +0800

    Basic Variaable
    Create and display variables.

---
+++ Variable_demo2.py
@@ -0,0 +1,8 @@
+# Create two variables; top_speed and distance.
+top_speed = 100
+distance = 300
+
+# Display the values referenced by the variables.
+print ("The top speed is", top_speed)
+print ("The distance traveled is", distance)
+
