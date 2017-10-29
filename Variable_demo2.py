6c0287332493fddb63701f3ebfc91d9861cce9bc
WenLong <hkivanleeon68sem2@gmail.com>
2017-10-29 09:32:54 +0800

    Python

--- Variable_demo2.py
+++
@@ -1,8 +0,0 @@
-# Create two variables; top_speed and distance.
-top_speed = 100
-distance = 300
-
-# Display the values referenced by the variables.
-print ("The top speed is", top_speed)
-print ("The distance traveled is", distance)
-
