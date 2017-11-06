0e5893a2422887bfc5adb835767fe55b33fe1888
WenLong <hkivanleeon68sem2@gmail.com>
2017-11-06 11:08:15 +0800

    Python

---
+++ Functions/Function_demo.py
@@ -0,0 +1,9 @@
+# This program demonstrates a function.
+# First, we define a function named message.
+
+def message():
+    print("I am WenLong,")
+    print("King of the Britons.")
+
+# Call the message function.
+message()
