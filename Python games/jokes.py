b8ba3a7d926aa1809a97a64364a9594f0d2ba540
WenLong Wu <hkivanleeon68@gmail.com>
2019-09-16 16:30:41 -0400

    update

---
+++ Python games/jokes.py
@@ -0,0 +1,27 @@
+# Date: 16-09-2019
+# Name: WenLong Wu
+# Python programming practice
+
+# Jokes
+
+# This program is to tells a few different jokes to the user.
+
+def main():
+
+    haha = input("What do you get when you cross a snowman with a vampire?")
+
+    haha = input("Frostbite!")
+
+    haha = input("What do dentists call an astronaut's cavity?")
+
+    haha = input("A black hole!")
+
+    haha = input("Knock Knock.")
+
+    haha = input("Who's there?")
+
+    haha = input("Interrupting cow.")
+
+    haha = input("Interrupting cow wh-MOO!")
+
+main()
